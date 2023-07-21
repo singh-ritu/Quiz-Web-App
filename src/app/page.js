f709cc69ab7840b8fdd6ffeca4b0b6a82c982b4f
Ritu <singhritu241101@gmail.com>
2023-07-21 12:13:10 +0530

    question counter added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
-  return <div></div>;
+  const styles = {
+    display: "flex",
+    flexDirection: "column",
+    // alignItems: "center",
+    float: "right",
+  };
+  const [questionCounter, setQuestionCounter] = useState(1);
+
+  return (
+    <div style={styles}>
+      <div
+        style={{
+          backgroundColor: "rebeccapurple",
+          padding: "2rem",
+        }}
+      >
+        <h1>{"Question " + questionCounter + " / 5"}</h1>
+      </div>
+    </div>
+  );
 }
