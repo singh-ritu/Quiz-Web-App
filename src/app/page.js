0e8df37a79b90f016de3e178015be2c3df4fe57f
Ritu <singhritu241101@gmail.com>
2023-07-22 13:36:10 +0530

    questions counters are handled

--- src/app/page.js
+++ src/app/page.js
@@ -7,20 +7,49 @@ export default function Home() {
   const styles = {
     display: "flex",
     flexDirection: "column",
-    // alignItems: "center",
     float: "right",
   };
   const [questionCounter, setQuestionCounter] = useState(1);
+
+  const handleNextQuestion = () => {
+    if (questionCounter < 5) setQuestionCounter(questionCounter + 1);
+    else alert("no more questions");
+  };
 
   return (
     <div style={styles}>
       <div
         style={{
           backgroundColor: "rebeccapurple",
-          padding: "2rem",
+          padding: "16px",
         }}
       >
         <h1>{"Question " + questionCounter + " / 5"}</h1>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          position: "fixed",
+          right: "100px",
+          bottom: "80px",
+        }}
+      >
+        {questionCounter > 1 && (
+          <button>
+            <h2> LAST QUESTION</h2>
+          </button>
+        )}
+        <button
+          style={{
+            backgroundColor: "rebeccapurple",
+            padding: "16px 32px",
+            borderRadius: "6px",
+            marginLeft: "30px",
+          }}
+          onClick={handleNextQuestion}
+        >
+          <h2> {questionCounter === 5 ? "SUBMIT" : "NEXT QUESTION"} </h2>
+        </button>
       </div>
     </div>
   );
---
+++ src/app/utils.js
@@ -0,0 +1,27 @@
+export const questions = [
+  {
+    question: "What is your Name ?",
+    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
+    answer: "Ritu",
+  },
+  {
+    question: "What is your Name ?",
+    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
+    answer: "Ritu",
+  },
+  {
+    question: "What is your Name ?",
+    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
+    answer: "Ritu",
+  },
+  {
+    question: "What is your Name ?",
+    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
+    answer: "Ritu",
+  },
+  {
+    question: "What is your Name ?",
+    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
+    answer: "Ritu",
+  },
+];
