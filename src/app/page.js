81d4200c737a62896f7e8160faea9a70f04ad9bc
Ritu <singhritu241101@gmail.com>
2023-08-10 23:40:09 +0530

    toggling the stepbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,37 @@
 "use client";
 
 import { useState } from "react";
-import { question } from "./utils";
+import { QuizQs, question } from "./utils";
 import { Image } from "next/image";
 import { questions } from "./utils";
 
 export default function Home() {
   const [questionCounter, setQuestionCounter] = useState(1);
+  const [currentOption, setCurrentOption] = useState(-1);
+  const [clickedOptions, setClickedOptions] = useState([]);
 
   const handleNextQuestion = () => {
     if (questionCounter < 5) setQuestionCounter(questionCounter + 1);
     else alert("no more questions");
+    setCurrentOption(-1);
   };
 
   const handleLastQuestion = () => {
     if (questionCounter >= 2) setQuestionCounter(questionCounter - 1);
+  };
+  const currentQuestion = QuizQs[questionCounter - 1];
+
+  const handleOptions = (index) => {
+    console.log(index);
+    const newvalues = [...clickedOptions];
+    newvalues[questionCounter - 1] = index;
+    setClickedOptions(newvalues);
+    console.log(clickedOptions);
+    setCurrentOption(index);
+  };
+  const selectedOptionStyle = {
+    borderColor: "rgb(43, 211, 10) ",
+    borderRadius: "4px",
   };
 
   return (
@@ -26,47 +43,39 @@ export default function Home() {
           height={500}
           alt="question"
         ></img>
-      </div>
-      <div className="right-container">
-        <div className="question-counter">
-          <h3>{"Question " + questionCounter + " / 5"}</h3>
-        </div>
-        <div>
-          {console.log(questions[questionCounter - 1])}
-          <h1>{questions[questionCounter - 1].question}</h1>
-          {questions[questionCounter - 1].options.map((option) => (
-            <h3>{option}</h3>
+        <div className="stepper ">
+          {QuizQs.map((question, index) => (
+            <div
+              className={
+                "stepbox" + " " + questionCounter - 1 === index ? "bar" : " "
+              }
+            ></div>
           ))}
         </div>
-        <div
-          style={{
-            display: "flex",
-            position: "fixed",
-            right: "100px",
-            bottom: "80px",
-          }}
-        >
-          {questionCounter > 1 && (
-            <button
-              style={{
-                backgroundColor: "lightgrey",
-                padding: "16px 32px",
-                borderRadius: "6px",
-              }}
-              onClick={handleLastQuestion}
+      </div>
+      <div className="right-container">
+        <div className="question-tag">
+          <h3>{"Question" + questionCounter + " / 5"}</h3>
+        </div>
+        <div className="Quiz-Questions">
+          <h1 className="Ques">{currentQuestion.question}</h1>
+          {currentQuestion.options.map((option, index) => (
+            <div
+              className="option selected_option"
+              onClick={() => handleOptions(index)}
+              style={currentOption === index ? selectedOptionStyle : {}}
             >
+              <h3 key={index}>{option}</h3>
+            </div>
+          ))}
+        </div>
+        <div className="button ">
+          {questionCounter > 1 && (
+            <button className="last-question-btn" onClick={handleLastQuestion}>
               <h2> LAST QUESTION</h2>
             </button>
           )}
-          <button
-            style={{
-              backgroundColor: "rebeccapurple",
-              padding: "16px 32px",
-              borderRadius: "6px",
-              marginLeft: "30px",
-            }}
-            onClick={handleNextQuestion}
-          >
+          <button className="next-question-btn" onClick={handleNextQuestion}>
             <h3> {questionCounter === 5 ? "SUBMIT" : "NEXT QUESTION"} </h3>
           </button>
         </div>
--- src/app/utils.js
+++ src/app/utils.js
@@ -1,27 +1,27 @@
-export const questions = [
+export const QuizQs = [
   {
     question: "What is your Name ?",
     options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
     answer: "Ritu",
   },
   {
-    question: "What is your Name ?",
-    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
-    answer: "Ritu",
+    question: "What is your Age ?",
+    options: ["24", "26", "28", "30"],
+    answer: "24",
   },
   {
-    question: "What is your Name ?",
-    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
-    answer: "Ritu",
+    question: "What is your country Name ?",
+    options: ["Russia", "Sweden", "India", "Mongolia"],
+    answer: "India",
   },
   {
-    question: "What is your Name ?",
-    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
-    answer: "Ritu",
+    question: "What is your favorite chocolate ?",
+    options: ["Five Star", "Perk", "Dairy milk", "KitKat"],
+    answer: "Perk",
   },
   {
-    question: "What is your Name ?",
-    options: ["Ritu", "Shashank", "Vaidhei", "Mansi"],
-    answer: "Ritu",
+    question: "What is your favorite brand ?",
+    options: ["chanel", "vercace", "louis vitton", "Gucci"],
+    answer: "Gucci",
   },
 ];
